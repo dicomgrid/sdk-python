879ab7f5eb9416320c6e49b67fd1507a05abf4ce
Kapustin Alexander <akapustin@ambrahealth.com>
2021-05-21 12:54:26 +0300

    Squashed commit of the following:
    commit f89eccc54f2de26a3ec8395eb2b8a5417d7f4c11
    Author: Kapustin Alexander <akapustin@ambrahealth.com>
    Date:   Fri May 21 12:53:27 2021 +0300
    
        Fixed storage version api

--- ambra_sdk/__init__.py
+++ ambra_sdk/__init__.py
@@ -5,7 +5,7 @@ API_VERSION = 'LBL0022 v45.0 2021-04-07'
 # equal for API_VERSION based on dictionary.html
 MODELS_VERSION = 'LBL0022 v45.0 2021-04-07'
 # from storage_api.html
-STORAGE_VERSION = 'LBL0038 v15.0 2021-05-19'
+STORAGE_VERSION = 'LbL0038 v15.0 2021-05-19'
 
 DOCS_URL = 'https://dicomgrid.github.io/sdk-python'
 ADDON_DOCS_URL = '{docs_url}/addon.html'.format(docs_url=DOCS_URL)
